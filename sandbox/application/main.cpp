108c842a4d087fe0e0624141971aa03102d71859
Tim <exceptioncast@outlook.com>
2026-07-13 22:29:26 +0300

    Vulkan device implementation

--- sandbox/application/main.cpp
+++ sandbox/application/main.cpp
@@ -3,7 +3,6 @@ import std;
 int main()
 {
     std::cout << "Hello, Sonora Engine!" << std::endl;
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
 
     sonora::engine::rhi::vulkan::VkDevice device;
     device.Initialize();
